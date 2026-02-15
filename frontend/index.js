ff57577eb16b7d76befa34526a9d534a48e5a316
Srikar <srikar2452@gmail.com>
2026-02-15 09:59:15 -0500

    This is my second commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,7 @@
 // this is initial commit for frontend
+// THis is the frontend code for the project
+// We will use React for the frontend development
+// This is the entry point for the React application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
