12742456329617f88104efa696acfa93669e08f3
Srikar-Gandikota <srikar2452@gmail.com>
2026-02-15 10:07:26 -0500

    Updated index.js

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,8 @@
 // THis is the frontend code for the project
 // We will use React for the frontend development
 // This is the entry point for the React application
+
+// well this is what i am doing in browser!!!!
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
